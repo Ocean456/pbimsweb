c27baf2498b2c621a0c84befef45b4c52d689eb8
22827 <2282724674@qq.com>
2023-06-06 00:45:37 +0800

    Merge remote-tracking branch 'origin/master'

--- src/components/Domicile.vue
+++ src/components/Domicile.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="domicile">
     <h3>户籍信息管理</h3>
-    <el-table border>
+    <el-table border style="width: 90%">
       <el-table-column label="身份证号"></el-table-column>
       <el-table-column label="姓名"></el-table-column>
       <el-table-column label="性别"></el-table-column>
@@ -15,6 +15,7 @@
       <el-table-column label="教育程度"></el-table-column>
       <el-table-column label="婚姻状况"></el-table-column>
       <el-table-column label="职业"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
     </el-table>
   </div>
 </template>
--- src/components/User.vue
+++ src/components/User.vue
@@ -2,27 +2,39 @@
 import store from "../store/index.ts";
 import {ElMessage, ElNotification} from "element-plus";
 import axios from 'axios'
+import {mapGetters} from "vuex";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/identity'
+  baseURL: 'http://localhost:8080/api'
 })
 
 export default {
   computed: {
     store() {
       return store
+    },
+    ...mapGetters(["currentUser", "isLoggedIn", "MenuUI"]),
+    username() {
+      return this.currentUser ? this.currentUser.username : "";
     }
   },
   data() {
     return {
-      id: '',
-      name: '',
-      gender: '',
-      nation: '',
-      birthday: '',
-      address: '',
-      active: 'info',
-      username: '1234'
+      info: {
+        id: '',
+        name: '',
+        gender: '',
+        nation: '',
+        birthday: '',
+        address: '',
+      },
+      migrate: {
+        id: '',
+        address: '',
+        date: new Date(),
+        reason: ''
+      },
+      active: 'info'
     }
   },
   methods: {
@@ -34,16 +46,20 @@ export default {
       })
     },
     check() {
-      api.get('/personal', {params: {username: this.username}})
+      api.get('/identity/personal', {params: {username: this.username}})
           .then(response => {
             ElMessage({
               type: "success",
               message: response.data
             })
           })
+    },
+    submitMigrate() {
+
     }
   },
   mounted() {
+    this.check();
   }
 }
 </script>
@@ -56,29 +72,29 @@ export default {
           <h3 class="Personal">居民个人信息登记</h3>
           <el-form class="user-form" label-position="right" label-width="100px">
             <el-form-item label="身份证号">
-              <el-input v-model="id" style="width: 90%;"></el-input>
+              <el-input v-model="info.id" style="width: 90%;"></el-input>
             </el-form-item>
             <el-form-item label="姓名">
-              <el-input v-model="name" style="width: 90%; "></el-input>
+              <el-input v-model="info.name" style="width: 90%; "></el-input>
             </el-form-item>
             <el-form-item label="性别">
-              <el-select v-model="gender" placeholder="男" style="width: 90%; ">
+              <el-select v-model="info.gender" placeholder="男" style="width: 90%; ">
                 <el-option label="男" value="男"/>
                 <el-option label="女" value="女"/>
               </el-select>
             </el-form-item>
             <el-form-item label="民族">
-              <el-input v-model="nation" style="width: 90%; "></el-input>
+              <el-input v-model="info.nation" style="width: 90%; "></el-input>
             </el-form-item>
             <el-form-item label="出生日期">
               <el-date-picker
-                  v-model="birthday"
+                  v-model="info.birthday"
                   style="width: 90%"
                   type="date"
               />
             </el-form-item>
             <el-form-item label="地址">
-              <el-input v-model="address" style="width: 90%; "></el-input>
+              <el-input v-model="info.address" style="width: 90%; "></el-input>
             </el-form-item>
           </el-form>
           <div class="buttonBox" style="display: flex">
@@ -92,7 +108,30 @@ export default {
         证件申请
       </el-tab-pane>
       <el-tab-pane label="迁移管理" name="migrate">
-        迁移申请
+        <el-card class="card">
+          <h3 style="margin-left: 40%">迁移信息管理</h3>
+          <el-form class="migrate-form" label-position="top" label-width="100px">
+            <el-form-item label="身份证号" label-width="130px">
+              <el-input v-model="migrate.id" disabled style="width: 90%;height: 40px"></el-input>
+            </el-form-item>
+            <el-form-item label="新的迁移地点" label-width="130px">
+              <el-input v-model="migrate.address" placeholder="请输入您要迁移的地点"
+                        style="width: 90%;height: 40px"></el-input>
+            </el-form-item>
+            <el-form-item label="迁移时间" label-width="130px">
+              <el-date-picker
+                  v-model="migrate.date"
+                  placeholder="请选择您的迁移日期"
+                  style="width: 90%;height: 40px"
+                  type="date"
+              />
+            </el-form-item>
+            <el-form-item label="迁移原因" label-width="130px">
+              <el-input v-model="migrate.reason" style="width: 90%" type="textarea"></el-input>
+            </el-form-item>
+            <el-button @click="submitMigrate">提交</el-button>
+          </el-form>
+        </el-card>
       </el-tab-pane>
       <el-tab-pane label="系统设置" name="setting">
         系统设置
